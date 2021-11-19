c9445e1fd76f58e4ada57b164a899745cdf1259d
Marco Pisasale <marc.93@hotmail.it>
2021-11-19 11:17:44 +0100

    FE Dockerfile

--- fe/src/main.js
+++ fe/src/main.js
@@ -6,8 +6,9 @@ import i18n from './i18n'
 import vuetify from './plugins/vuetify';
 import axios from 'axios';
 import '@/plugins/apexcharts';
+import config from './config.json';   // required for dockerized version of the app (can't load .env file vars)
 
-axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL ? process.env.VUE_APP_BASE_URL : config.VUE_APP_BASE_URL;
 Vue.config.productionTip = false
 
 new Vue({
